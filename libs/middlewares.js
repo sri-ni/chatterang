866ed3aaccb300d9c7ff8a7cc30440f6c33087ef
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-09-15 11:25:20 -0500

    CRUDification of books API done.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,13 @@
+import bodyParser from 'body-parser';
+
 module.exports = app => {
   app.set('port', 3000);
   app.set('json spaces', 2);
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    if (req && req.body && req.body.id) {
+      delete req.body.id;
+    }
+    next();
+  });
 };
--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,48 @@
 module.exports = app => {
   const Books = app.db.models.Books;
-  app.get('/books', (req, res) => {
-    Books.findAll({}).then(books => {
-      res.json({books: books});
+
+  app.route('/books')
+    .get((req, res) => {
+      Books.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .post((req, res) => {
+      Books.create(req.body)
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
     });
-  });
+
+  app.route('/books/:id')
+    .get((req, res) => {
+      Books.findOne({where: req.params})
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .put((req, res) => {
+      Books.update(req.body, {where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .delete((req, res) => {
+      Books.destroy({where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        })
+    });
 };
