51f761a22cab913b4ccf2521bd05eeefc5d7f5f7
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-09-16 10:07:57 -0500

    #2 Adding documentation for a Clients resource API.

--- routes/clients.js
+++ routes/clients.js
@@ -10,6 +10,28 @@ module.exports = app => {
         next();
       }
     })
+    /**
+     * @api {get} /clients List all the Clients
+     * @apiPermission admin
+     * @apiGroup Clients
+     * @apiVersion 0.0.1
+     * @apiHeader {String} Authorization Token of authenticated user
+     * @apiHeaderExample {json} Header
+     *    {"Authorization": "JWT xyz.abc.123.hgf"}
+     * @apiSuccess {Object[]} clients Client's list
+     * @apiSuccess {Number} clients.id Client ID
+     * @apiSuccess {String} clients.name Client Name
+     * @apiSuccess {String} clients.secret Client Secret
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    [{
+     *      "id": 1,
+     *      "name": "app one",
+     *      "secret": "$2a$10$AQFRm0GLXqAX.JJEl.ArGVlic8D27.nJDEUblR2"
+     *    }]
+     * @apiErrorExample {json} Authorization error
+     *    HTTP/1.1 401 Unauthorized
+     */
     .get((req, res) => {
       Clients.findAll({})
       .then(result => res.json(result))
