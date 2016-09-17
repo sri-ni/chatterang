5f76f71f63a7f3036adb5f70cf87bbe02a05970e
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-09-17 17:24:51 -0500

    #4 Added logging at API and SQL level using morgan and winston.

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,9 +1,14 @@
+import logger from './logger';
+
 module.exports = {
   database: "savantorybooks",
   username: "groot",
   password: "iamgroot",
   params: {
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: (sql) => {
+      logger.info(`[${new Date()}] ${sql}`);
+    }
   },
   jwtSecret: "sav@nt0ry-b0oOks-@#!",
   jwtSession: {session: false}
--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,10 +1,19 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import logger from './logger';
 
 module.exports = app => {
   app.set('port', 3000);
   app.set('json spaces', 2);
+  app.use(morgan('common', {
+    stream: {
+      write: (message) => {
+        logger.info(message);
+      }
+    }
+  }));
   app.use(cors({
     origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
