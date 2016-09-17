0e500ec12278c36feb5d9e1358ad86bb52951238
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-09-17 17:01:24 -0500

    #3 CORS support added to API.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,15 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 module.exports = app => {
   app.set('port', 3000);
   app.set('json spaces', 2);
+  app.use(cors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
