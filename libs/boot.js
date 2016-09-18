52f374597064e64b6e56024bda3be30a9d4d99fe
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-09-17 21:48:37 -0500

    #6 SSL support to make service HTTPS completed.

--- libs/boot.js
+++ libs/boot.js
@@ -1,9 +1,17 @@
+import https from 'https';
+import fs from 'fs';
+
 module.exports = app => {
   if (process.env.NODE_ENV !== 'test') {
+    const credentials = {
+      key: fs.readFileSync('savantory-books.key', 'utf8'),
+      cert: fs.readFileSync('savantory-books.cert', 'utf8')
+    };
     app.db.sequelize.sync().done(() => {
-      app.listen(app.get('port'), () => {
-        console.log(`savantory-books microservice - port ${app.get('port')}`);
-      });
+      https.createServer(credentials, app)
+        .listen(app.get('port'), () => {
+          console.log(`savantory-books microservice - port ${app.get('port')}`);
+        });
     });
   } else {
     app.db.sequelize.sync().done(() => {
